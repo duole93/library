9ba1970e717fe813e931eb1da267f197c494e3ae
Duong Le <duole93@live.com>
2024-01-13 20:24:53 -0600

    create book object, properties, and more.
    - create book object
    - create books arrays
    - create modal for add buton
    - add eventListener to add button to show modal
    - add eventListener too close button to close modal
    - add display function to display book title, author, pages, and reading
    status on html
    - add reset display function to reset input after submit form
    or close modal

--- script.js
+++ script.js
@@ -1,6 +1,16 @@
 //DOM reference
-const addBtn = document.querySelector(".add > button");
+const addaBookBtn = document.querySelector(".add > button");
+//Dialog References
+const newBookDialog = document.querySelector("#new-book-dialog");
+const titleDialog = document.querySelector("#title");
+const authorDialog = document.querySelector("#author");
+const pagesDialog = document.querySelector("#pages");
+const isReadDialog = document.querySelector("#isRead");
 
+const closeDialogBtn = document.querySelector("#new-book-dialog > form > .cancel-btn");
+const submitaBookBtn = document.querySelector("#new-book-dialog > form > .add-btn");
+
+const bookContainer = document.querySelector(".book-container");
 
 function Book(title, author, pages, isRead) {
     this.title=title;
@@ -8,28 +18,76 @@ function Book(title, author, pages, isRead) {
     this.pages=pages;
     this.isRead=isRead;
 }
-Book.prototype.getTitle = function () {
-    return this.title;
-}
+// Book.prototype.getTitle = function () {
+//     return this.title;
+// }
 
-Book.prototype.getAuthorname = function () {
-    return this.author;
-}
+// Book.prototype.getAuthorname = function () {
+//     return this.author;
+// }
 
-Book.prototype.getTotalPages = function () {
-    return this.pages;
-}
+// Book.prototype.getTotalPages = function () {
+//     return this.pages;
+// }
 
-Book.prototype.getReadStatus = function () {
-    return this.isRead;
-}
+// Book.prototype.getReadStatus = function () {
+//     return this.isRead;
+// }
 
 //list of book
 const books = new Array();
 
-addBtn.addEventListener('click', () =>{
-    let book = new Book('naruto', 'managaka', 700*18, true);
-    books.push(book);
+//display book - clear display, and display book
+
+function displayBooks(){
+    //clear display;
+    bookContainer.innerHTML="";
+
+    //display the list again
+    books.forEach((item, index)=>{
+        let bookCard= document.createElement("div");
+        let title= document.createElement("h1");
+        title.textContent = item.title;
+        let author= document.createElement("p");
+        author.textContent = item.author;
+        let pages= document.createElement("p");
+        pages.textContent = item.pages;
+        let isRead= document.createElement("p"); 
+        if(item.isRead)
+            isRead.textContent = 'Competed';
+        else
+            isRead.textContent = 'inprogress';
+        bookCard.append(title, author, pages, isRead);
+        bookCard.classList.add("book-card");
+        bookCard.classList.add(`${index}`)
+        bookContainer.appendChild(bookCard);
+    });
+}
+function resetDialog(){
+    titleDialog.value = "";
+    authorDialog.value ="";
+    pagesDialog.value="";
+    isReadDialog.checked=false;
+}
+addaBookBtn.addEventListener('click', () =>{
+    newBookDialog.showModal();
 })
 
+submitaBookBtn.addEventListener('click',(e)=>{
+    e.preventDefault();
+    let title = titleDialog.value;
+    let author = authorDialog.value;
+    let page = pagesDialog.value;
+    let isRead = isReadDialog.checked;
+    let book = new Book (title, author, page, isRead);
+    books.push(book);
+    newBookDialog.close();
+    displayBooks();
+    resetDialog();
+})
 
+closeDialogBtn.addEventListener('click', (e)=>{
+    e.preventDefault();
+    newBookDialog.close();
+    resetDialog();
+})
